8f28da053214dfd4555976f2f5593150d834ccdb
Shaun Kirk <shaun.kirk@gmail.com>
2024-06-09 17:03:36 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const pastes = require("./data/pastes-data");
+const pastesRouter = require("./pastes/pastes.router");
+const usersRouter = require("./users/users.router");
+
+app.use(express.json());
+
+app.use("/pastes", pastesRouter); // Note: app.use
+app.use("/users", usersRouter);
+
+// Not found handler
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
+
+module.exports = app;
